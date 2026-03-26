c70ccfc25406255ef7500ca2ad6c5ad412f19f8f
harithasudheesh <haritha601974@gmail.com>
2026-03-26 11:47:12 +0530

    update settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(h4=r*gjl*)d7g&4-o4cpc+6+&3pd7ru+wx1a$8p+m@vk!)ujy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',"https://ecommercesite-5m7q7v5ri-harithasudheeshs-projects.vercel.app"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
